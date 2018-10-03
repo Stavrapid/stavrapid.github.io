35a5f1e5d161c94d37dd494c36a4d5ae9521ff47
Stavrapid <stavrapid@mail.ru>
2018-10-04 01:11:30 +0300

    Add files via upload

--- pages/YellowTable/script/menu_hide.js
+++ pages/YellowTable/script/menu_hide.js
@@ -1,8 +1,11 @@
 window.onscroll = function() {
 	var scrolled = window.pageYOffset || document.documentElement.scrollTop;
 	var banner = document.getElementsByClassName("header__banner")[0];
+	var el = document.getElementsByTagName('html')[0];
+	var style = window.getComputedStyle(el, null).getPropertyValue('font-size');
+	var fontSize = parseFloat(style);
 	window.y = scrolled;
-	if (window.y >= (banner.offsetHeight - 5 * 18)) {
+	if (window.y >= (banner.offsetHeight - 5 * fontSize)) {
 		fixMenu();
 	} else {
 		unfixMenu();
@@ -12,6 +15,7 @@ window.onscroll = function() {
 function fixMenu() {
 	var header = document.getElementsByClassName('header')[0];
 	header.classList.add("_fixed");
+	// document.getElementsByClassName("header__banner")[0].style.display = "none";
 }
 
 function unfixMenu() {
