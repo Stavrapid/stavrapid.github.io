7ed493d7e648718677ddcb6c4cd956866aac2779
Stavrapid <stavrapid@mail.ru>
2018-10-20 04:37:22 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -182,9 +182,10 @@ window.onload = function () {
 		case "complete":
 			// The page is fully loaded.
 			// console.log("The first CSS rule is: " + document.styleSheets[0].cssRules[0].cssText);
-			document.getElementById("stavrapid_logo").style.animation = "fade-out 1s both cubic-bezier(.42,.17,.28,1)";
-			document.getElementById("main_page").style.display = "block";
-			console.log("loaded");
+			setTimeout(function () {
+				document.getElementById("stavrapid_logo").style.animation = "fade-out 1s both cubic-bezier(.42,.17,.28,1)";
+				document.getElementById("main_page").style.display = "block";
+			}, 1000);
 			break;
 	}
 }
